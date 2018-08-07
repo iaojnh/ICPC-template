053f746803daf5c2501e96122c6711343b12b877
iaojnh <38692278+iaojnh@users.noreply.github.com>
2018-08-07 20:22:17 +0800

    Update 回文树.cpp

--- 字符串/回文树.cpp
+++ 字符串/回文树.cpp
@@ -4,32 +4,32 @@ namespace Palindromic_Tree
 	char s[N];
 	int size[N],len;
 	struct Node {
-        int son[M];
-        int ff,len;
-    }t[N];
-    int last,tot;
-    void init(char *a) {
-        last=0,tot=1;
-		len=strlen(a);
-		rep(i,0,len+1)s[i]=a[i];
-		memset(t,0,sizeof(Node)*(len+5));
-		memset(size,0,sizeof(int)*(len+5));
-        t[tot++].len=-1;
-        t[0].ff=t[1].ff=1;
-    }
-    void build() {
-        rep(i,0,len) {
-		int c=s[i]-'a';
-		int p=last;
-		while(s[i-t[p].len-1]!=s[i])p=t[p].ff;
-		if(!t[p].son[c]) {
-		    int v=tot++,k=t[p].ff;
-		    t[v].len=t[p].len+2;
-		    while(s[i-t[k].len-1]!=s[i])k=t[k].ff;
-		    t[v].ff=t[k].son[c];
-		    t[p].son[c]=v;
-		}
-		last=t[p].son[c];
-		size[t[p].son[c]]++;
+		int son[M];
+		int ff,len;
+	}t[N];
+	int last,tot;
+	void init(char *a) {
+	    last=0,tot=1;
+	    len=strlen(a);
+	    rep(i,0,len+1)s[i]=a[i];
+	    memset(t,0,sizeof(Node)*(len+5));
+	    memset(size,0,sizeof(int)*(len+5));
+	    t[tot++].len=-1;
+	    t[0].ff=t[1].ff=1;
 	}
+	void build() {
+		rep(i,0,len) {
+			int c=s[i]-'a';
+			int p=last;
+			while(s[i-t[p].len-1]!=s[i])p=t[p].ff;
+			if(!t[p].son[c]) {
+				int v=tot++,k=t[p].ff;
+				t[v].len=t[p].len+2;
+				while(s[i-t[k].len-1]!=s[i])k=t[k].ff;
+				t[v].ff=t[k].son[c];
+				t[p].son[c]=v;
+			}
+			last=t[p].son[c];
+			size[t[p].son[c]]++;
+		}
 };
